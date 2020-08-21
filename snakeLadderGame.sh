0d811ca0f6a91515ed34fc9a529b3306c217904d
soumen-gif <soumentaki1996@gmail.com>
2020-08-21 23:18:26 +0530

    Added the check for option functionality

--- snakeLadderGame.sh
+++ snakeLadderGame.sh
@@ -2,10 +2,27 @@ echo "welcome to snake ladder game"
 startPosition=0
 finishPosition=100
 
-POSITION=0
+hisPosition=0
+option(){
+	choice=$((RANDOM % 3))
+	echo "he got ....$choice"
+	if [ $choice -eq 2 ]
+	then
+		echo "Ladder"
+		hisPosition=$(($hisPosition + $dieRoll))
+	elif [ $choice -eq 1 ]
+	then
+		echo "Snake"
+		hisPosition=$(($hisPosition - $dieRoll))
+	else
+		echo "No play"
+	fi
+}
 dice()
 {
-	dieRoll=$((RANDOM % 6 + 1))
-	echo "he got the..... $dieRoll"
+        dieRoll=$((RANDOM % 6 + 1))
+        echo "he got ..$dieRoll"
+	option
 }
 dice
+echo now his  position is $hisPosition
